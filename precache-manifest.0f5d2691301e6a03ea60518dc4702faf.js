f30b0bf81083845db3180976667804fce2cfa3af
Your Name <your.email@example.com>
2023-07-24 19:40:21 +0500

    Updates

---
+++ precache-manifest.0f5d2691301e6a03ea60518dc4702faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb24882f6d349553903c",
+    "url": "/React-App-Todo/static/css/main.2d394685.chunk.css"
+  },
+  {
+    "revision": "cb24882f6d349553903c",
+    "url": "/React-App-Todo/static/js/main.cb24882f.chunk.js"
+  },
+  {
+    "revision": "3c75360870ee2223b36c",
+    "url": "/React-App-Todo/static/js/1.3c753608.chunk.js"
+  },
+  {
+    "revision": "49fcef33f7fc23d7ffa8",
+    "url": "/React-App-Todo/static/js/2.49fcef33.chunk.js"
+  },
+  {
+    "revision": "ccdd305ab05adb6c359f",
+    "url": "/React-App-Todo/static/js/runtime~main.ccdd305a.js"
+  },
+  {
+    "revision": "b4b7a688ee881f78b43f13c202b771dc",
+    "url": "/React-App-Todo/index.html"
+  }
+];
